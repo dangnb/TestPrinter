0e08a0bd4ca7e8f8ebb89caf5f918c4185d72d7a
dangnb <dangnb@esct.vn>
2024-04-16 17:58:05 +0700

    dangnb commit

---
+++ appprinter/lib/image_utils.dart
@@ -0,0 +1,40 @@
+import 'package:image/image.dart' as imglib;
+
+class ImageUtils{
+ static splitImage(List<int> input ){
+      // convert image to image from image package
+  imglib.Image? image = imglib.decodeImage(input);
+  if (image != null) {
+    int width = image.width;
+    int height = image.height;
+    var split = 100;
+    bool full = image.height % split == 0;
+    int n = height % split == 0 ? height ~/ split : (height ~/ split) + 1;
+    // split image to parts
+    List<imglib.Image> parts = [];
+    for (int i = 0; i < n; i++) {
+      imglib.Image b;
+      if (full) {
+        b = imglib.copyCrop(image, 0, i * split, width, split);
+      } else if (i == n - 1) {
+        b = imglib.copyCrop(image, 0, i * split, width, height - i * split);
+      } else {
+        b = imglib.copyCrop(image, 0, i * split, width, split);
+      }
+      b = imglib.grayscale(b);
+      b = imglib.grayscale(b);
+      b = imglib.grayscale(b);
+      parts.add(b);
+    }
+
+    // convert image from image package to Image Widget to display
+    // List<imglib.Image>? output = [];
+    // for (var img in parts) {
+    //   output.add(imglib.memory(imglib.encodeJpg(img)));
+    // }
+    return parts;
+  }
+  return null;
+
+  }
+}
---
+++ appprinter/lib/print_screent.dart
@@ -0,0 +1,74 @@
+import 'package:appprinter/image_utils.dart';
+import 'package:esc_pos_utils/esc_pos_utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bluetooth_printer/flutter_bluetooth_printer.dart';
+
+class MyWidget extends StatelessWidget {
+  MyWidget({super.key});
+  Widget build(BuildContext context) {
+    return StreamBuilder<dynamic>(
+        stream: FlutterBluetoothPrinter.discovery,
+        builder: (context, snapshot) {
+          final list = snapshot.data ?? <BluetoothDevice>[];
+          return ListView.builder(
+              itemCount: list.length,
+              itemBuilder: (context, index) {
+                final device = list.elementAt(index);
+                return ListTile(
+                    title: Text(device.name ?? 'No Name'),
+                    subtitle: Text(device.address),
+                    onTap: ()async {
+                      await print(device);
+                    });
+              });
+        });
+  }
+
+  Future<void> print(BluetoothDevice divice) async {
+    final ByteData data = await rootBundle.load('assets/logo.png');
+    var imagePrint = ImageUtils.splitImage(Uint8List.view(data.buffer));
+    if (imagePrint != null) {
+      CapabilityProfile profile = await CapabilityProfile.load();
+      Generator generator = Generator(PaperSize.mm58, profile);
+      List<int> bytes = [];
+      int space = 12;
+      if (imagePrint.length > space) {
+        double count = imagePrint.length / space;
+        if (count - count.toInt() > 0) {
+          count = count + 1;
+        }
+        int index = 0;
+        while (index < count.toInt()) {
+          List<int> byte = [];
+          for (int i = index * space;
+              i <
+                  (index == count.toInt() - 1
+                      ? imagePrint.length
+                      : (index + 1) * space);
+              i++) {
+            byte += generator.imageRaster(imagePrint[i]);
+          }
+          await FlutterBluetoothPrinter.printBytes(
+            address: divice.address,
+            keepConnected: true,
+            data: Uint8List.fromList(byte),
+          );
+          index++;
+        }
+        bytes = [];
+      } else {
+        for (var element in imagePrint) {
+          bytes += generator.imageRaster(element);
+        }
+      }
+      bytes += generator.feed(3);
+      bytes += generator.cut();
+      await FlutterBluetoothPrinter.printBytes(
+        address: divice.address,
+        keepConnected: true,
+        data: Uint8List.fromList(bytes),
+      );
+    }
+  }
+}
